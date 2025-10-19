8f9791ade2f191e9f5ae470503514dd2bbb836ff
Abdallah Kareem <162487911+abdallahkareem@users.noreply.github.com>
2025-10-19 08:09:31 +0300

    Update Parser.java
    updated parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,13 +1,24 @@
-
 public class Parser {
     String commandName;
     String[] args;
-
+    
+    public Parser() {
+    	commandName = "";
+    }
+    
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
     public boolean parse(String input) {
-		return false;
-    	
+    	boolean isParsed = false;
+		String[] tokens = input.split(" ");
+		args = new String[tokens.length - 1];
+
+		commandName = tokens[0];
+		for(int i = 1 ; i < tokens.length ; i++) {
+			args[i - 1] = tokens[i];
+			isParsed = true;
+		}
+    	return isParsed;
     }
     public String getCommandName(){
 		return commandName;
