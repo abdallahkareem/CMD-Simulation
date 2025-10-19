e7e9765d8b9cd930570ac121776b93e30ebd07ea
Abdallah Kareem <162487911+abdallahkareem@users.noreply.github.com>
2025-10-19 08:09:04 +0300

    Update Main.java
    small parser

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("I can commit to this at any time");
-	}
-
+	
+    public static void main(String[] args) {
+       Parser pp = new Parser();
+       pp.parse("echo Abdallah Kareem");
+       System.out.println(pp.getCommandName());
+       System.out.println();
+       String[] ss = pp.getArgs();
+       for(String rr : ss) {
+    	   System.out.println(rr);
+       }
+    }
 }
